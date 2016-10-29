8d9ca5a2d3ea0d2ddfc8e06c302d21439da131f5
F.Lovera <floriana.lovera@gmail.com>
2016-10-29 14:26:53 +0200

    Initial commit

---
+++ script_sql/eva_create_db.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE "EVA_db"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'it_IT.UTF-8'
+       LC_CTYPE = 'it_IT.UTF-8'
+       CONNECTION LIMIT = -1;
---
+++ script_sql/eva_create_table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE public.eva
+(
+  eva_nr integer NOT NULL,
+  country character varying(40) NOT NULL,
+  crew character varying(200) NOT NULL,
+  vehicle character varying(100) NOT NULL,
+  date_ date NOT NULL,
+  duration interval minute to second,
+  purpose character varying NOT NULL,
+  CONSTRAINT firstkey PRIMARY KEY (eva_nr)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.eva
+  OWNER TO postgres;
